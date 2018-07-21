d4b6ba9b4cc1c11a7796abbf8323b0cdae73e5e3
Felix Chan <felixchan29@gmail.com>
2018-07-21 01:13:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
 def oxford_comma(array)
-  array[0,array.length-1].join(", ") << ", and #{array[-1]}"
+  if array.legth < 3 
+    array.join(" and ")
+  else
+    array[0,array.length-1].join(", ") << ", and #{array[-1]}"
+  end
 end
